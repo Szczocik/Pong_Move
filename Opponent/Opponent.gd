5e23b2bdfec998aa90a5fa1f913a7c66621b5de6
Mariusz Popa <popa.mariusz@gmail.com>
2022-04-29 05:56:36 -0400

    opponent direction

---
+++ Opponent/Opponent.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+var speed = 250
+var ball
+
+
+func _ready():
+	ball = get_parent().find_node("Ball")
+
+
+func _physics_process(delta):
+	move_and_slide(Vector2(get_opponent_direction() * speed, 0))
+	
+	
+func get_opponent_direction():
+	if abs(ball.position.x - position.x) > 25:
+		if ball.position.x > position.x: return 1
+		else: return -1
+	else: return 0
+	
