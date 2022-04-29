5fc80e23df7b879653eaa9af67ea977580f510bb
Mariusz Popa <popa.mariusz@gmail.com>
2022-04-29 10:48:02 -0400

    Game_over

--- Level/Level.gd
+++ Level/Level.gd
@@ -1,11 +1,13 @@
 extends Node
 
-var PlayerScore = 0
+var PlayerScore = 3
 
 
 func _on_Bottom_body_entered(body):
 	$Ball.position = Vector2(300,512)
 	PlayerScore -= 1
+	if PlayerScore <= 0:
+		game_over()
 	get_tree().call_group("BallGroup","stop_ball")
 	$CountdownTimer.start()
 	$CountdownLabel.visible = true
@@ -27,3 +29,7 @@ func _process(delta):
 func _on_CountdownTimer_timeout():
 	get_tree().call_group("BallGroup","restart_ball")
 	$CountdownLabel.visible = false
+
+
+func game_over():
+	get_tree().change_scene("res://Level/Game_over.tscn")
