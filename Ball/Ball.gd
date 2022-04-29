2e4dd8c21312afe2a560fa6aca76a625f9bc5c0f
Mariusz Popa <popa.mariusz@gmail.com>
2022-04-29 07:50:23 -0400

    CountdownTimer

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -12,3 +12,13 @@ func _physics_process(delta):
 	var collision_object = move_and_collide(velocity * speed * delta)
 	if collision_object:
 		velocity = velocity.bounce(collision_object.normal)
+
+
+func stop_ball():
+	speed = 0
+
+
+func restart_ball():
+	speed = 400
+	velocity.x = [-0.8,0.8][randi() % 2]
+	velocity.y = [-1,1][randi() % 2]
--- Level/Level.gd
+++ Level/Level.gd
@@ -1,10 +1,30 @@
 extends Node
 
+var PlayerScore = 0
+var OpponentScore = 0
+
 
 
 func _on_Bottom_body_entered(body):
 	$Ball.position = Vector2(300,512)
-
+	OpponentScore += 1
+	get_tree().call_group("BallGroup","stop_ball")
+	$CountdownTimer.start()
+	$CountdownLabel.visible = true
 
 func _on_Top_body_entered(body):
 	$Ball.position = Vector2(300,512)
+	PlayerScore += 1
+	get_tree().call_group("BallGroup","stop_ball")
+	$CountdownTimer.start()
+	$CountdownLabel.visible = true
+
+func _process(delta):
+	$PlayerScore.text = str(PlayerScore)
+	$OpponentScore.text = str(OpponentScore)
+	$CountdownLabel.text = str(int($CountdownTimer.time_left) + 1)
+
+
+func _on_CountdownTimer_timeout():
+	get_tree().call_group("BallGroup","restart_ball")
+	$CountdownLabel.visible = false
