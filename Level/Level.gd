28a8ddec518e270cdb68207dd20550ff82bea77f
Mariusz Popa <popa.mariusz@gmail.com>
2022-04-29 10:00:54 -0400

    Sound

--- Level/Level.gd
+++ Level/Level.gd
@@ -1,16 +1,15 @@
 extends Node
 
 var PlayerScore = 0
-var OpponentScore = 0
-
 
 
 func _on_Bottom_body_entered(body):
 	$Ball.position = Vector2(300,512)
-	OpponentScore += 1
+	PlayerScore -= 1
 	get_tree().call_group("BallGroup","stop_ball")
 	$CountdownTimer.start()
 	$CountdownLabel.visible = true
+	$ScoreSound.play()
 
 func _on_Top_body_entered(body):
 	$Ball.position = Vector2(300,512)
@@ -18,11 +17,11 @@ func _on_Top_body_entered(body):
 	get_tree().call_group("BallGroup","stop_ball")
 	$CountdownTimer.start()
 	$CountdownLabel.visible = true
+	$ScoreSound.play()
 
 func _process(delta):
 	$PlayerScore.text = str(PlayerScore)
-	$OpponentScore.text = str(OpponentScore)
-	$CountdownLabel.text = str(int($CountdownTimer.time_left) + 1)
+	$CountdownLabel.text = str(int($CountdownTimer.time_left) +1)
 
 
 func _on_CountdownTimer_timeout():
