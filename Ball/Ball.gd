8db119d99777a8cbcaa56a18209343f411713cd3
Mariusz Popa <popa.mariusz@gmail.com>
2022-04-29 05:37:05 -0400

    Ball

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -1,11 +1,14 @@
 extends KinematicBody2D
 
-var speed = 600
+var speed = 400
 var velocity = Vector2.ZERO
 
 func _ready():
+	randomize()
 	velocity.x = [-0.8,0.8][randi() % 2]
 	velocity.y = [-1,1][randi() % 2]
 	
 func _physics_process(delta):
-	move_and_slide(velocity * speed)
+	var collision_object = move_and_collide(velocity * speed * delta)
+	if collision_object:
+		velocity = velocity.bounce(collision_object.normal)
