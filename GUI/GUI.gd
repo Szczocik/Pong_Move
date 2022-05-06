a29f647f386bd3cf452607a81f42ea9bcb2cff5c
Mariusz Popa <popa.mariusz@gmail.com>
2022-05-06 04:09:40 -0400

    GUI

--- GUI/GUI.gd
+++ GUI/GUI.gd
@@ -1,2 +1,7 @@
 extends CanvasLayer
 
+func _ready():
+	$Control/TextureRect/HBoxContainer/PlayerScore.text = "3"
+	
+func update_GUI(PlayerScore):
+	$Control/TextureRect/HBoxContainer/PlayerScore.text = str(PlayerScore)
--- Level/Level.gd
+++ Level/Level.gd
@@ -5,13 +5,15 @@ extends Node
 var PlayerScore = 3
 
 func _ready():
-	add_to_group("Gamestate")
+	update_GUI()
+	
 
 
 
 func _on_Bottom_body_entered(body):
 	$Ball.position = Vector2(300,512)
 	PlayerScore -= 1
+	update_GUI()
 	if PlayerScore <= 0:
 		game_over()
 	get_tree().call_group("BallGroup","stop_ball")
@@ -22,20 +24,25 @@ func _on_Bottom_body_entered(body):
 func _on_Top_body_entered(body):
 	$Ball.position = Vector2(300,512)
 	PlayerScore += 1
+	update_GUI()
 	get_tree().call_group("BallGroup","stop_ball")
 	$CountdownTimer.start()
 	$CountdownLabel.visible = true
 	$ScoreSound.play()
 	if PlayerScore > 4:
 		get_tree().call_group("BallGroup","speed_ball")
+		update_GUI()
 	
 	if PlayerScore == 10:
 		get_tree().change_scene("res://Level_1/Level_1.tscn")
-		PlayerScore 
+		update_GUI() 
 		
+func update_GUI():
+	get_tree().call_group("GUI", "update_GUI", PlayerScore)
+
 
 func _process(delta):
-	$PlayerScore.text = str(PlayerScore)
+	#$PlayerScore.text = str(PlayerScore)
 	$CountdownLabel.text = str(int($CountdownTimer.time_left) +1)
 
 
