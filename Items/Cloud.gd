4ab52cc32fb58d4b5b82c9880633c6a74274213d
Mariusz Popa <popa.mariusz@gmail.com>
2022-05-10 04:01:27 -0400

    Cloud

--- Items/Cloud.gd
+++ Items/Cloud.gd
@@ -4,7 +4,12 @@ var cloud_speed = 100
 
 func _process(delta):
 	position.y += cloud_speed * delta
+	if position.y >= 300:
+		$Sprite.modulate = Color(0.45,0.43,0.43,1)
+	if position.y >= 800:
+		$Sprite.modulate = Color(0.53,0.02,0.02,1)
 	
 
 func _on_Cloud_body_entered(body):
-	pass
+	get_tree().call_group("Gamestate", "game_over")
+	
